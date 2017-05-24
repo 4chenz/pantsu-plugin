bc000f8752199a9a9b77bb1cab5a5569d7f62ba2
4chenz <4chenz@users.noreply.github.com>
2017-05-24 10:24:17 -0600

    updated plugins

--- sukebei.py
+++ sukebei.py
@@ -1,4 +1,4 @@
-#VERSION: 1.03
+#VERSION: 1.04
 #AUTHORS: anon
 import re
 from helpers import retrieve_url, download_file
@@ -18,8 +18,9 @@ class sukebei(object):
         print(download_file(info))
     def search(self, what, cat='all'):
         page=1
+        per_page=100
         while True:
-            url = 'https://sukebei.pantsu.cat/search/{}?c='.format(page)+self.supported_categories[cat]+'&q='+what+'&max=50'
+            url = 'https://sukebei.pantsu.cat/search/{}?c='.format(page)+self.supported_categories[cat]+'&q='+what+'&max='+str(per_page)
             print(url)
             url = retrieve_url(url)
             regex_things = re.findall(r'<tr class="torrent-info(.*?)</tr>', url, re.M|re.I|re.S)
@@ -28,14 +29,19 @@ class sukebei(object):
                 for url in re.findall(r'href=[\'"]?([^\'" >]+)', regex_things[x]):
                     if 'magnet' in url:
                         dic['link']=url
-                dic['name']=re.findall(r'<td class="name">(.*?)</a>', regex_things[x], re.M|re.I|re.S)[0].split('\n')[2]
-                dic['size']=re.findall(r'<td class="hidden-xs filesize">(.*?)</td>', regex_things[x], re.M|re.I|re.S)[0]#.replace('[','').replace(']','')
-                dic['seeds']=re.findall(r'<b class="text-success">(.*?)</b>', regex_things[x], re.M|re.I|re.S)[0]
-                dic['leech']=re.findall(r'<b class="text-danger">(.*?)</b>', regex_things[x], re.M|re.I|re.S)[0]
+                dic['name'] = re.findall(r'<td class="name">(.*?)</a>', regex_things[x], re.M|re.I|re.S)[0].split('\n')[2].replace('                      ', '')
+                dic['size'] = re.findall(r'<td class="hidden-xs nowrap">(.*?)</td>', regex_things[x], re.M|re.I|re.S)[0]
+                dic['seeds']= re.findall(r'<b class="text-success">(.*?)</b>', regex_things[x], re.M|re.I|re.S)
+                if dic['seeds'] == []:
+                    dic['seeds']=-1
+                    dic['leech']=-1
+                else:
+                    dic['leech']= re.findall(r'<b class="text-danger">(.*?)</b>', regex_things[x], re.M|re.I|re.S)[0]
+                    dic['seeds']=dic['seeds'][0]
                 dic['engine_url']=self.engine_url
                 prettyPrinter(dic)
             page+=1
-            if len(regex_things) < 50:
+            if len(regex_things) < per_page:
                 break
 
 
